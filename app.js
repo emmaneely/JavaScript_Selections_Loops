bc026663071a6f97bf2cf6698dfb2f08b9e1a2ff
emmaneely <emmaneely3@gmail.com>
2022-06-08 18:03:11 -0500

    Completed exercise 2

--- app.js
+++ app.js
@@ -13,3 +13,16 @@ for (let i = 1; i <= 100; i++) {
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FIZZBUZZ");
+    } else if (i % 3 == 0) {
+        console.log("FIZZ");
+    } else if (i % 5 == 0) {
+        console.log("BUZZ");
+    }
+    else {
+        console.log(i);
+    }
+}
