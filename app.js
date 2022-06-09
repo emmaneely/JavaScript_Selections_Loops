7b859682eb390d400a324db32f6a4aafe59f15b5
emmaneely <emmaneely3@gmail.com>
2022-06-08 19:14:20 -0500

    Completed exercise 3

--- app.js
+++ app.js
@@ -26,3 +26,52 @@ for (let i = 1; i <= 100; i++) {
         console.log(i);
     }
 }
+
+// Exercise 3
+console.log("EXERCISE 3:\n==========\n");
+
+let i = 1;
+while (i <= 100) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+    i++;
+}
+
+let j = 1;
+do {
+    if (j % 2 !== 0) {
+        console.log(j);
+    }
+    j++;
+} while (j <= 100);
+
+let k = 1;
+while (k <= 100) {
+    if (k % 3 == 0 && k % 5 == 0) {
+        console.log("FIZZBUZZ");
+    } else if (k % 3 == 0) {
+        console.log("FIZZ");
+    } else if (k % 5 == 0) {
+        console.log("BUZZ");
+    }
+    else {
+        console.log(k);
+    }
+    k++;
+}
+
+let l = 1;
+do {
+    if (l % 3 == 0 && l % 5 == 0) {
+        console.log("FIZZBUZZ");
+    } else if (l % 3 == 0) {
+        console.log("FIZZ");
+    } else if (l % 5 == 0) {
+        console.log("BUZZ");
+    }
+    else {
+        console.log(l);
+    }
+    l++;
+} while (l <= 100);
